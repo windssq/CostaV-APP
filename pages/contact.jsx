3fc60717efa6f5e4653e7723121a52c6b21d3930
windssq <166147548+windssq@users.noreply.github.com>
2024-06-02 21:29:39 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -16,26 +16,26 @@ export default function Home() {
             <div className="max-w-lg w-full">
                 <a target="_blank" rel="norefferer" href="https://www.youtube.com/@devil.sq0">
                     <div className="flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">Youtube</p>
+                        <i className="fab fa-youtube fa-2x" />
+                        <p className="font-semibold text-xl">Youtube Kanalı</p>
                     </div>
                 </a>
                 <a target="_blank" rel="norefferer" href="https://top.gg/bot/1244620556641239060">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-facebook fa-2x" />
+                        <i className="fab fa-top.gg fa-2x" />
                         <p className="font-semibold text-xl">Top.gg Oy Ver</p>
                     </div>
                 </a>
                 <a target="_blank" rel="norefferer" href="Developer">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">Karta</p>
+                        <p className="font-semibold text-xl">Destek Sunucusu</p>
                     </div>
                 </a>
                 <a href="mailto:https://discord.gg/costav">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
-                        <p className="font-semibold text-xl">Destek Sunucusu</p>
+                        <p className="font-semibold text-xl">costavyedekk@gmail.com</p>
                     </div>
                 </a>
             </div>
