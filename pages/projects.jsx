35a905dc053a0290aabda07a346400834cf27866
windssq <166147548+windssq@users.noreply.github.com>
2024-06-02 21:43:23 +0300

    Update projects.jsx

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -15,14 +15,14 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="">
+            <a href="https://discord.com/oauth2/authorize?client_id=1244620556641239060">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://i.hizliresim.com/e9mwrqg.jpg" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Partner Yok !!!</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">CostaV</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                      Partner Yok !!!
+                                      CostaV Developer Karta
                                     </p>
                                 </div>     
                             </div>
@@ -30,25 +30,6 @@ export default function Partners() {
                     </a>
                     </div>
 
-
-                            <div className="flex flex-col justify-center text-white rounded">
-            <a href="">
-                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://i.hizliresim.com/e9mwrqg.jpg" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Partner Yok!!!</h1>        
-                                <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                   Şuanda Partner Yok!!!
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                    </a>
-                    </div>
-
-                </div>
-                <div className="py-10"></div>
         </>
     );
 };
